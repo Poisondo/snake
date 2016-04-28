e91b71747e017ce1586d6dcc0745e2ca83fdad5b
Oleg <poison.d28@gmail.com>
2016-04-28 18:00:09 +0300

    Создан класс Snake

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -22,12 +22,43 @@ namespace Snake
             y = _y;
             sym = _sym;
         }
+        
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
 
+        public void Move(int offset, Direction direction)
+        {
+            switch (direction)
+            {
+                case Direction.RIGHT:
+                    x = x + offset;
+                    break;
+                case Direction.LEFT:
+                    x = x - offset;
+                    break;
+                case Direction.DOWN:
+                    y = y + offset;
+                    break;
+                case Direction.UP:
+                    y = y - offset;
+                    break;
+                default:
+                    break;
+            }
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
         }
 
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
+        }
     }
 }
